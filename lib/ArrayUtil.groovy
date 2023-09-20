37224abed60dc1eb2f7742fcedf6f0ed6e4e10df
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-09-20 13:57:52 +0200

    Fix method signature

--- lib/ArrayUtil.groovy
+++ lib/ArrayUtil.groovy
@@ -1,6 +1,6 @@
 class ArrayUtil {
 
-    public static String sort(Collection data) {
+    public static String sort(Object data) {
        def dataArray = data as String[]
        def dataSorted = dataArray.sort(false) { it }
        def dataJoined = dataSorted.join(" ")
